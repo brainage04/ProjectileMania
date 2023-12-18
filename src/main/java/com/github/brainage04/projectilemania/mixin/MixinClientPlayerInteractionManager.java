486521583bc009f3ddcd17006870d015f7280eb5
brainage04 <thomasmcmah21@gmail.com>
2023-12-18 18:40:18 +1000

    cleanup v2

--- src/main/java/com/github/brainage04/projectilemania/mixin/MixinClientPlayerInteractionManager.java
+++ src/main/java/com/github/brainage04/projectilemania/mixin/MixinClientPlayerInteractionManager.java
@@ -11,7 +11,6 @@ import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
